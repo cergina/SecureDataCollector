ea6eaae86032fb6e3d6d847a2b9a5592ab0b1e60
Tomáš Zátka <zatka@kascomp.sk>
2021-03-21 17:55:25 +0100

    ControllerUnit: funkcia pre tvorbu a poslanie packetu po uart-e

--- firmware/ControllerUnit/ControllerUnit/ControllerUnit.c
+++ firmware/ControllerUnit/ControllerUnit/ControllerUnit.c
@@ -1,11 +1,10 @@
 /*
  * ControllerUnit.c
  * Project: DCS
- * Version: 0.1
+ * Version: 0.2
  * Controller: ATmega8 
  * Author: Bc. Tomas Zátka
  */ 
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,12 +40,104 @@
 
 #define CHUNK_LEN 50
 
-int main(void)
+// Function to convert integer to character array
+char* convertIntegerToChar(int N)
 {
+	int m = N;
+	int digit = 0;
+	while (m) {
+		digit++;
+		m /= 10;
+	}
+	
+	char* arr;
+	char arr1[digit];
+	arr = (char*)malloc(digit);
+	int index = 0;
+	
+	while (N) {
+		arr1[++index] = N % 10 + '0';
+		N /= 10;
+	}
+	
+	int i;
+	for (i = 0; i < index; i++) {
+		arr[i] = arr1[index - i];
+	}
+	arr[i] = '\0';
+	return (char*)arr;
+}
+
+void SendMessageToCEU_uart(unsigned char UID, short input_NO, int value){
+	char * message = (char* )calloc(CHUNK_LEN, sizeof(char));
+	int mlen = 0; // 1B of stx + 2B of length
+	
+	message[0] = STX;
+	//LENGTH (init)
+	message[++mlen] = 0;
+	message[++mlen] = 0;
+	
+	//DATA PART (in ascii)	
+	//Identifikátor kontroleru
+	char temp[4];
+	sprintf(temp,"%x",UID);
+	message[++mlen] = temp[0];
+	message[++mlen] = temp[1];
+	//Input
+	if(input_NO){
+		message[++mlen] = 0x49; // I - input
+		message[++mlen] = input_NO  + '0'; // We want charcode
+	}
+	//Data message (M only, ...)
+	///Type
+	message[++mlen] = 0x4D; // M - Measument
+	///data
+	char *cDataArr = calloc(20, sizeof(char));
+	sprintf(cDataArr,"%d",value);
+	int cDatalength= strlen(cDataArr);
+	
+	for(int i = 0; i < strlen(cDataArr); i++){
+		//Message length check
+		if(mlen == CHUNK_LEN){
+			message = realloc(message, mlen+CHUNK_LEN * sizeof(message));
+		}
+		message[++mlen] = cDataArr[i];
+	}
+	free(cDataArr);
+	
+	//Add length
+	int packetLength = mlen - 2;
+	message[1] = packetLength & 0xFF;
+	message[2] = packetLength >> 8;	
+	
+	//End of message
+	message[++mlen] = ETX;
+	
+	//Checksum
+	int crc = crc8((uint8_t*)message, mlen);
+	message[++mlen] = crc;
+	message[++mlen] = '\0';
+	
+	//debug
+	for (int i = 0; i < mlen; i++)
+		uart_putc(message[i]);
+	
+	free(message);
+} 
+
+/*
+void SendMessageToCEU_zwave(int mData, char controller_ID, short input_NO){
+	
+}*/
+
+int main(void)
+{	
 	unsigned int c;
 	char* message = NULL;
 	unsigned short current_flag = NULL;
 	unsigned int index = 0;
+	
+	unsigned char UID = 0xFF; // for test purposes
 	
 	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
 
@@ -86,16 +177,9 @@ int main(void)
 		if (current_flag == F_CRC) {
 			int len = message[1] << 8 |  message[0];
 		
-			int y = len;
-			/* for debug
-			index = 2;
-			for (int i = 2; i <= len; i++)
-			uart_putc(message[i]);
-			*/
-			
+			int y = len + 2;
 			checksum = 0;
 			index = 0;
-			y = len + 2;
 			while(y-- > 0){
 				checksum ^= message[index++];
 			}
@@ -106,47 +190,13 @@ int main(void)
 				uart_putc(checksum);
 			}
 			else{
-				uart_puts("ACK"); 
+				uart_puts("ACK");
+				//do stuff
+				SendMessageToCEU_uart(UID,1,0); // for test purpose
 			}
 			current_flag = NULL;
+			free(message);
 		}
     }	
 }
 
-
-void SendMessageToCEU_uart(int mData, char controller_ID, short input_NO){
-	char * message;
-	
-	message = (char* )calloc(CHUNK_LEN, sizeof(char));
-	
-	message[0] = STX;
-	
-	//length
-	message[1]
-	message[2]
-	
-	//Data part (in ascii)
-	
-	//CE ID
-	message[3]
-	message[4]
-	//input
-	
-	//data
-	
-	
-	// crc
-	int length // message length
-	
-	/* index = 0;
-	while(y-- > 0){
-		checksum ^= message[index++];
-	}
-	*/
-	int crc = crc8(message, length);
-} 
-
-
-void SendMessageToCEU_zwave(int mData, char controller_ID, short input_NO){
-	
-}
