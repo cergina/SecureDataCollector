46c4bca0dd280dfa50194b06f6d1a1edfd5c0652
Maros Cerget <mcerget@gmail.com>
2021-03-29 16:17:28 +0200

    HTML and CSS files for authentication servlets

--- src/main/webapp/static/js/collector.js
+++ src/main/webapp/static/js/collector.js
@@ -37,7 +37,7 @@ function createUser() {
         statusCode: {
             201: function(response) {
                 $(':input').val('');
-                alert('Verfikacny kod je: ' + response.data.verificationcode);
+                alert('Verifikacny kod je: ' + response.data.verificationcode);
             },
             400: function(jqXHR) {
                 var response = JSON.parse(jqXHR.responseText);
@@ -66,7 +66,7 @@ function registerUser() {
         statusCode: {
             200: function(response) {
                 // TODO thank user for registration
-                alert('Thank you for your reigstration' + response.message)
+                alert('Thank you for your registration\n' + response.message)
             },
             401: function(jqXHR) {
                 var response = JSON.parse(jqXHR.responseText);
