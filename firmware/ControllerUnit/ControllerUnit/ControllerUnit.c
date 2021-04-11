723bb72e2aa00022b4b04db479729d7c666e64c1
Tomáš Zátka <zatka@kascomp.sk>
2021-04-11 18:31:45 +0200

    ControllerUnit: citanie analog pinu ADC, implementovaný interupt pre PCINT

--- firmware/ControllerUnit/ControllerUnit/ControllerUnit.c
+++ firmware/ControllerUnit/ControllerUnit/ControllerUnit.c
@@ -29,6 +29,8 @@
 
 #include "util/delay.h"	
 
+#define DEBUG
+
 #define UART_BAUD_RATE 9600
 
 #define STX 0x02
@@ -50,6 +52,14 @@
 #define LOW_UP 164
 #define HIGH_DOWN 410
 #define HIGH_UP 1023
+
+// Control flags for ADC5 (PCINT13)
+uint16_t ADC5_lastValue;
+volatile short int ADC5_readFlag = 0; // True/false
+
+ISR(PCINT1_vect){
+	ADC5_readFlag = 1;
+}
 
 void SendMessageToCEU_uart(unsigned char UID, short input_NO, int value){
 	char * message = (char* )calloc(CHUNK_LEN, sizeof(char));
@@ -109,106 +119,115 @@ void SendMessageToCEU_uart(unsigned char UID, short input_NO, int value){
 void SendMessageToCEU_zwave(int mData, char controller_ID, short input_NO){
 }*/
 
-
-ISR(PCIN13){
-	uint16_t adc_value = adc_read(ADC_PRESCALER_128, ADC_VREF_AVCC, 5);
-	
-	char result[50];
-	sprintf(result, "%d", adc_value);
-	
-	uart_puts(result);
-	
-	if (adc_value >= HIGH_DOWN)
-	uart_puts("INT citam\r\n");
-	
-	if (adc_value <= LOW_UP)
-	uart_puts("INT necitam\r\n");
-}
-
-
-
 int main(void)
 {	
 	unsigned int c;
 	char* message = NULL;
 	unsigned short current_flag = NULL;
 	unsigned int index = 0;
-	
+	char checksum;
 	unsigned char UID = 0xAA; // for test purposes
 	
+	PCICR |= (1 << PCIE1);     // set PCIE1 to enable PCMSK1 scan
+	PCMSK1 |= (1 << PCINT13);   // set PCINT13 to trigger an interrupt on state change
+
 	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
 	sei();
-	uart_puts("ControllerUnit  Build v0.6 \r\n");
+	uart_puts("ControllerUnit  Build v0.46 \r\n");
 	
-	//SendMessageToCEU_uart(UID,1,10); // for test purpose
+	//Init value ADC5 - PCINT13
+	ADC5_lastValue = adc_read(ADC_PRESCALER_128, ADC_VREF_AVCC, 5);
+	#ifdef DEBUG
+		char result[50];
+		sprintf(result, "%d", ADC5_lastValue);
+		uart_puts(result);
+		uart_puts("\r\n");
+	#endif
+	ADC5_lastValue = 1; // We begin with logical 1 bcs on device its means idle state
 	
-	char checksum;
+	int tick= 0;
 	
 	while(1) {
+		//Process code for ADC5 (PCINT13)
+		if(ADC5_readFlag == 1){
+			uint16_t adc_value = adc_read(ADC_PRESCALER_128, ADC_VREF_AVCC, 5);
+			uart_puts("Measure\r\n");
+			uint8_t bValue = adc_value >= HIGH_DOWN ? 1 : (adc_value <= LOW_UP ? 0 : -1);
+			
+			// RISE UP
+			if (bValue != ADC5_lastValue && bValue == 1 ){
+				#ifdef DEBUG
+					uart_puts("Rising edge\r\n");
+				#endif
+				//ignore
+			}
+			
+			if (bValue != ADC5_lastValue && bValue == 0 ){
+				#ifdef DEBUG
+					uart_puts("Falling edge\r\n");
+				#endif
+				//We send 1 as measured value because for now its trigger every time
+				SendMessageToCEU_uart(UID,1,1); // for test purpose
+			}
+			
+			ADC5_lastValue = bValue;
+			ADC5_readFlag = 0;
+		}
+		
+		#ifdef DEBUG
+			char result1[50];
+			sprintf(result1, "%d", ++tick);
+			uart_puts(result1);
+			uart_puts("\r\n");
+		#endif
 		_delay_ms(1000);
-		
-		uint16_t adc_value = adc_read(ADC_PRESCALER_128, ADC_VREF_AVCC, 5);
-		
-		char result[50];
-		sprintf(result, "%d", adc_value);
-		
-		uart_puts(result);
-		
-		if (adc_value >= HIGH_DOWN)
-		  		uart_puts("citam\r\n");
-				  
-		if (adc_value <= LOW_UP)
-		  		uart_puts("necitam\r\n");
-	}
-	
-	
-	/*
-	while(1){
+		/*
 		c = uart_getc();
 		if (c & UART_NO_DATA){
 			continue;
 		}
-	
-		if (c == STX){
-			current_flag = F_DATA;
-			index = 0;
-			message = (char* )calloc(CHUNK_LEN, sizeof(char));
-			continue;
-		}
-	
-		if (c == ETX){
-			current_flag = F_CRC;
-			continue;
-		}
-	
-		if (current_flag == F_DATA)
-		{
-			message[index] = (char)c;
-			index++;
-			if(index == CHUNK_LEN){
-				int len = sizeof message / sizeof *message;
-				message = realloc(message, len+CHUNK_LEN * sizeof(message));
+		else{		
+			if (c == STX){
+				current_flag = F_DATA;
+				index = 0;
+				message = (char* )calloc(CHUNK_LEN, sizeof(char));
+				continue;
 			}
-		}
-	
-		if (current_flag == F_CRC) {
-			int len = message[1] << 8 |  message[0];
 		
-			checksum = crc8((uint8_t*)message, len);
-			if(c != checksum){
-				uart_puts("Wrong Checksum \r\n");
-				uart_putc(c);
-				uart_putc(checksum);
+			if (c == ETX){
+				current_flag = F_CRC;
+				continue;
 			}
-			else{
-				uart_puts("ACK");
-				//do stuff
-				//
-				//SendMessageToCEU_uart(UID,1,10); // for test purpose
+			
+			if (current_flag == F_DATA)
+			{
+				message[index] = (char)c;
+				index++;
+				if(index == CHUNK_LEN){
+					int len = sizeof message / sizeof *message;
+					message = realloc(message, len+CHUNK_LEN * sizeof(message));
+				}
 			}
-			current_flag = NULL;
-			free(message);
-		}
-    }*/
+			
+			if (current_flag == F_CRC) {
+				int len = message[1] << 8 |  message[0];
+				
+				checksum = crc8((uint8_t*)message, len);
+				if(c != checksum){
+					uart_puts("Wrong Checksum \r\n");
+					//uart_putc(c);
+					//uart_putc(checksum);
+				}
+				else{
+					uart_puts("ACK");
+					//do stuff
+				}
+				current_flag = NULL;
+				free(message);
+			}
+		}*/
+	}
 }
+		
+
 
