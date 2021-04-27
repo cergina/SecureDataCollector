7aeea4100e4b0690445e4da0b848220cb5168d46
ludko <ludovit@popelka.com>
2021-04-27 15:41:58 +0200

    resolve conflicts

--- src/main/java/View/Web/Servlets/Privileged/UserSpecific/ProjectsInformationServlet.java
+++ src/main/java/View/Web/Servlets/Privileged/UserSpecific/ProjectsInformationServlet.java
@@ -1,16 +1,16 @@
 package View.Web.Servlets.Privileged.UserSpecific;
 
 import Control.ConfigClass;
-import Control.Scenario.UC_ListProject;
-import Control.Scenario.UC_ListUser;
 import Control.Scenario.UC_Addresses;
+import Control.Scenario.UC_ProjectListing;
+import Control.Scenario.UC_UserListing;
+import Model.Database.Support.CustomLogs;
 import Model.Web.Address;
 import Model.Web.Project;
 import Model.Web.User;
 import View.Configuration.ContextUtil;
 import View.Support.DcsWebContext;
 import View.Support.ServletAbstracts.SessionServlet;
-import View.Support.ServletHelper;
 import View.Support.SessionUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -33,6 +33,8 @@ public class ProjectsInformationServlet extends SessionServlet {
     private static final String VARIABLE_PROJECT = "project";
     private static final String VARIABLE_USERS = "users";
     private static final String VARIABLE_ADDRESS_TYPES = "addresses";
+
+    private static final String REQUEST_PARAM_ID = "id";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -59,11 +61,11 @@ public class ProjectsInformationServlet extends SessionServlet {
         List<Project> projects;
         if (SessionUtil.getIsadmin(request.getSession(false))) {
             // all projects for admin
-            projects = (new UC_ListProject(getDb())).allProjects();
+            projects = (new UC_ProjectListing(getDb())).allProjects();
         } else {
             // own projects for user
             User user = SessionUtil.getUser(request.getSession(false));
-            projects = (new UC_ListProject(getDb())).allProjectsForUser(user.getUserID());
+            projects = (new UC_ProjectListing(getDb())).allProjectsForUser(user.getUserID());
         }
 
         context.setVariable(VARIABLE_PROJECTS, projects);
@@ -77,29 +79,34 @@ public class ProjectsInformationServlet extends SessionServlet {
         WebContext context = DcsWebContext.WebContextInitForDCS(request, response,
                 ConfigClass.HTML_VARIABLENAME_RUNNINGREMOTELY, trueIfRunningRemotely);
 
-        Integer requestedId = ServletHelper.getRequestParamId(request);
-        if (requestedId == null) {
+        int requestedProjectId;
+        try {
+            requestedProjectId = Integer.parseInt(request.getParameter(REQUEST_PARAM_ID));
+            CustomLogs.Development("V requeste prisiel id: " + requestedProjectId);
+        } catch (NumberFormatException nfe) {
+            CustomLogs.Error("Bad request or nothing came into server as ?id=[number should be here]");
             response.sendError(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
 
-        UC_ListProject uc = new UC_ListProject(getDb());
+        UC_ProjectListing uc = new UC_ProjectListing(getDb());
         HttpSession session = request.getSession(false);
         if (!SessionUtil.getIsadmin(session)) { // is not admin?
             User user = SessionUtil.getUser(session);
-            if (!uc.isUserInProject(requestedId, user.getUserID())) { // is not authorized to see?
+            if (!uc.isUserInProject(requestedProjectId, user.getUserID())) { // is not authorized to see?
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
                 return;
             }
         }
-        Project project = uc.specificProject(requestedId);
+        Project project = uc.specificProject(requestedProjectId);
         if (project == null) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
-        List<User> users = (new UC_ListUser(getDb())).allUsersForProject(requestedId);
+        List<User> users = (new UC_UserListing(getDb())).allUsersForProject(requestedProjectId);
 
         final List<Address> addressList = (new UC_Addresses(getDb()).getAll_UnusedAddress());
+
 
         context.setVariable(VARIABLE_PROJECT, project);
         context.setVariable(VARIABLE_USERS, users);
